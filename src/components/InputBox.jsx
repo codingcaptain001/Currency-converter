6567ed3b97c4ca444c9cf999d7bf750202a535ce
codingcaptain001 <lakshyanamdev001@gamil.com>
2026-01-17 11:12:23 +0530

    comm

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -51,7 +51,7 @@ function InputBox({
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
-                    onChange={handleAmountChange}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
                     
